979c0cf936938eb1352f0bb6476a723b3840058d
Eeo Jun <141bytes@gmail.com>
2018-10-26 22:21:21 +0100

    update

--- client.py
+++ client.py
@@ -1,3 +1,5 @@
+import datetime
+import time
 import socket
 from server import recv_packet, send_packet
 
@@ -6,18 +8,28 @@ HOST = 'localhost'
 PORT = 8081
 
 
-def try_send(*args):
-    if not send_packet(*args):
+def try_send(sock, type, data):
+    if not send_packet(sock, type, data):
         print("Timeout: Server unavailable")
         exit(1)
 
 
-def try_recv(*args):
-    type, data = recv_packet(*args)
-    if type is None:
-        print("Timeout: Server unavailable")
-        exit(1)
-    return type, data
+def try_recv(sock, reason):
+    t0 = time.time()
+    with open('client.log', 'a') as fp:
+        type, data = recv_packet(sock)
+        t1 = time.time()
+        if type is None:
+            fp.write('[{0}] Timeout during {1}: server unavailable.\n'.format(datetime.datetime.now(), reason))
+            print("Timeout: Server unavailable")
+            exit(1)
+        fp.write('[{0}] Server response ({1}): length={2}, time={3}s.\n'.format(
+            datetime.datetime.now(),
+            reason,
+            len(type) + 1 + len(data),
+            t1 - t0,
+            ))
+        return type, data
 
 
 def main():
@@ -39,7 +51,7 @@ def main():
     try_send(s, b'REQ', artist.encode('ascii'))
     #_, _ = try_recv(s)
     #print('Server received query')
-    _, data = try_recv(s)
+    _, data = try_recv(s, 'fetch songs')
     songs = data.decode('ascii')
     print('Songs:')
     if songs:
@@ -50,7 +62,7 @@ def main():
 
     input('Quit? (Enter)')
     try_send(s, b'BYE', b'')
-    try_recv(s)
+    try_recv(s, 'close connection')
     s.close()
     print('Connection closed by server.')
 
